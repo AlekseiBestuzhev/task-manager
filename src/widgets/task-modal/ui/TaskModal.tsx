3d84ff055f36b601fdfb4e4960ae25209630fbf4
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-10-04 20:17:03 +0400

    upd: task modal ui done

--- src/widgets/task-modal/ui/TaskModal.tsx
+++ src/widgets/task-modal/ui/TaskModal.tsx
@@ -1,7 +1,15 @@
 import { TaskType } from 'features/tasks-management/api/tasks.api.types';
-import { Button, Typography } from '@mui/material';
+import { Button, TextField, Typography } from '@mui/material';
 import { Modal } from 'shared/components';
 import React, { FC } from 'react';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 type Props = {
 	onClose: () => void;
@@ -13,17 +21,44 @@ type Props = {
 export const TaskModal: FC<Props> = ({ onClose, open, title, task }) => {
 	return (
 		<Modal onClose={onClose} open={open} title={title}>
-			<Typography variant="body1" sx={{ maxWidth: '400px' }}>
-				Get information about this task or change different data you can here...
-			</Typography>
+			{/* <p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo sequi eveniet officiis commodi suscipit, nam in optio
+				nesciunt placeat beatae earum fuga minima tenetur voluptates quis esse veniam debitis quibusdam!
+			</p> */}
 			<form className="task-info">
-				<p>Title: {task.title}</p>
-				<p>Status: {task.status}</p>
-				<p>Priority: {task.priority}</p>
-				<p>Added date: {new Date(task.addedDate).toLocaleString()}</p>
-				<p>Start date: {task.startDate}</p>
-				<p>Deadline: {task.deadline}</p>
-				<p>Description: {task.description}</p>
+				<div className="task-info__created">
+					<span>Creted:</span> <span>{new Date(task.addedDate).toLocaleString()}</span>
+				</div>
+				<TextField value={task.title} label="Task title" />
+				<div className="task-info__select-group">
+					<FormControl className="select">
+						<InputLabel id="demo-simple-select-label" className="select__label">
+							Status
+						</InputLabel>
+						<Select labelId="demo-simple-select-label" id="demo-simple-select" value={task.status} label="Age">
+							<MenuItem value={10}>Ten</MenuItem>
+							<MenuItem value={20}>Twenty</MenuItem>
+							<MenuItem value={30}>Thirty</MenuItem>
+						</Select>
+					</FormControl>
+					<FormControl className="select">
+						<InputLabel id="demo-simple-select-label" className="select__label">
+							Priority
+						</InputLabel>
+						<Select labelId="demo-simple-select-label" id="demo-simple-select" value={task.priority} label="Age">
+							<MenuItem value={10}>Ten</MenuItem>
+							<MenuItem value={20}>Twenty</MenuItem>
+							<MenuItem value={30}>Thirty</MenuItem>
+						</Select>
+					</FormControl>
+				</div>
+				<LocalizationProvider dateAdapter={AdapterDayjs}>
+					<DemoContainer components={['DatePicker', 'DatePicker']}>
+						<DatePicker label="Start date" value={task.startDate} />
+						<DatePicker label="Deadline" value={task.deadline} />
+					</DemoContainer>
+				</LocalizationProvider>
+				<TextField value={task.description} label="Description" multiline />
 				<div className="task-buttons">
 					<Button variant="contained" color="inherit" onClick={onClose}>
 						Cancel
